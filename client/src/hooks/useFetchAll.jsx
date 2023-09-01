2240d6d68c843113933bdeabd477c984b6445459
Ozgi10 <129106787+Ozgi10@users.noreply.github.com>
2023-09-01 18:10:31 +0200

    Add app

--- client/src/hooks/useFetchAll.jsx
+++ client/src/hooks/useFetchAll.jsx
@@ -4,7 +4,9 @@ const useFetchAll = () => {
   const [allPets, setAllPets] = useState([]);
 
   useEffect(() => {
-    fetch("https://gist.github.com/abf8f94bcff14ace1b88c7977d651a74.git")
+    fetch(
+      "https://gist.githubusercontent.com/arturschaefer/abf8f94bcff14ace1b88c7977d651a74/raw/4c329530a24fe2e4b21029ed7a687a767aa9622a/breed_list.json"
+    )
       .then((response) => {
         if (response.status === 200) {
           setAllPets(response.data);
